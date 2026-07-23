2a07fb2da50c848dcd4b6a4d213086bc42de80e3
attaullahkhan234 <attaullahkhann703@gmail.com>
2026-07-23 19:46:07 +0200

    printing and statment

---
+++ print and statment.py
@@ -0,0 +1,12 @@
+# print a greeting message
+print("Hello world!")
+
+# printing my name
+print("My name is Attaullah.")
+
+# PRINTING A FUN FACT ABOUT MY SELF
+# print("i love playing soccer.")
+print("i love playing soccer.")
+
+# performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
