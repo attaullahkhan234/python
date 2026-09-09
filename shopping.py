a1e35ddc5b321a4a5f906b5edd18efc90e3ae421
attaullahkhan234 <attaullahkhann703@gmail.com>
2026-09-09 23:25:55 +0200

    shopping.py

---
+++ shopping.py
@@ -0,0 +1,51 @@
+
+file_name = "shopping_list.txt"
+shopping_file = open(file_name, "w")
+shopping_file.write("Shopping List\n")
+shopping_file.write("1. Milk\n")
+shopping_file.write("2. Bread\n")
+shopping_file.write("3. Eggs\n")
+shopping_file.write("4. Apples\n")
+shopping_file.close()
+print("Shopping list created successfully!")
+
+
+
+
+shopping_file = open(file_name, "r")
+print("\n===== Full Shopping List =====")
+content = shopping_file.read()
+print(content)
+shopping_file.close()
+
+
+
+
+shopping_file = open(file_name, "a")
+shopping_file.write("5. Rice\n")
+shopping_file.write("6. Butter\n")
+shopping_file.write("7. Juice\n")
+shopping_file.close()
+print("New  3 items added to the shopping list!")
+
+
+
+
+
+shopping_file = open(file_name, "r")
+print("\n===== Updated Shopping List =====")
+updated_content = shopping_file.read()
+print(updated_content)
+shopping_file.close()
+
+
+
+
+
+
+print("\n================================")
+print("SHOPPING LIST MANAGER SUMMARY")
+print("================================")
+print("File created:", file_name)
+print("Items were written, appended, and read successfully.")
+print("================================")
